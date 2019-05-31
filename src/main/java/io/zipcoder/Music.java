a542e8ce3202429af4cb1a963f2defcf94e5b879
Darya <Darya.yatchanka@gmail.com>
2019-05-31 18:18:24 -0400

    PlaylistChallangeDone

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,39 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        Integer count1 = 0;
+        Integer count2 = 0;
+
+        for (int i = startIndex; i < playList.length; i++) {
+            if (!playList[i].equals(selection)) {
+
+
+                count1 ++;
+
+
+            }
+        }
+        for (int j = playList.length-1; j > 0; j--) {
+            if (!playList[j].equals(selection)) {
+                count2 ++;
+
+            }
+        }
+
+
+        int min = Math.min(count1, count2);
+
+
+        return min;
     }
 }
